0aeaaff67ca02d974d852271c8439a94363c1330
Anas <xhinling288@gmail.com>
2026-03-03 10:44:28 +0500

    Add background

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,10 +1,37 @@
+import gsap from "gsap";
+import {useGSAP} from "@gsap/react";
+import ScrollTrigger from "gsap/all";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  useGSAP(() => {
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        start: 'bottom top',
+        scrub: 1
+      }
+    })
+
+    tl.fromTo(
+      "nav",
+      {
+        backgroundColor: "#4b3317",
+      },
+      {
+        backgroundColor: "#5a422610",
+        backdropFilter: "blur(10px)",
+        duration: 1,
+      },
+    );
+  })
+
   return (
     <nav>
       <div className="logo">
-        <img src="" alt="logo" />
+        <img src="/Images/logo.png" alt="logo" width={30} />
         <Link to="/">Fabricino</Link>
       </div>
       <div className="links">
